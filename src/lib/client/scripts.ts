923dbe872854944f6bec3e47e71a2e40e545778b
Gwon Hyuksang <gwonhyuksang@gmail.com>
2024-04-15 13:08:11 +0900

    fix

--- src/lib/client/scripts.ts
+++ src/lib/client/scripts.ts
@@ -8,7 +8,7 @@ export function createTransactionElement(d: TransactionDto) {
 	a.classList.add('px_2');
 
 	a.innerHTML = `
-					<span class="text-align_start">${toDateString(d.date)}</span>
+					<span class="text-align_start" data-date=${d.date}>${toDateString(d.date)}</span>
 					<span>${d.description}</span>
 					<span class="text-align_end">${d.amount.toLocaleString()}</span>
 				`;
@@ -23,15 +23,4 @@ export function toDateString(dateString: string) {
 	const month = date.getMonth() + 1;
 	const day = date.getDate();
 	return `${month}/${day}`;
-}
-
-export function toDate(dateString: string) {
-	if (dateString.length !== 4) {
-		return new Date(dateString);
-	}
-
-	const [month, day] = dateString.split('/');
-	const year = new Date().getFullYear();
-
-	return new Date(`${year}-${month!.padStart(2, '0')}-${day!.padStart(2, '0')}`);
 }
