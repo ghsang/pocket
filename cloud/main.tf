34786db9d02a7e6ba1b2768e76d96b427e4003c0
Gwon Hyuksang <gwonhyuksang@gmail.com>
2024-05-04 09:30:54 +0900

    feat: attach aws logging

--- cloud/main.tf
+++ cloud/main.tf
@@ -116,3 +116,36 @@ resource "aws_lambda_permission" "lambda_invoke" {
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.monthly_trigger.arn
 }
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  for_each = local.binaries
+
+  name              = "/aws/lambda/pocket/${each.key}"
+  retention_in_days = 3
+}
+
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
