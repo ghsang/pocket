07fd2ea0691de1988707e0b9d4e3603f2af14db6
Gwon Hyuksang <gwonhyuksang@gmail.com>
2024-05-22 22:45:08 +0900

    refactor: tailwindcss instead of pandacss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,60 +1,66 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import AstroPWA from '@vite-pwa/astro';
 import auth from 'auth-astro';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-		},
-	}),
-	vite: {
-		logLevel: 'info',
-		define: {
-			__DATE__: `'${new Date().toISOString()}'`,
-		},
-		server: {
-			fs: {
-				allow: ['../..'],
-			},
-		},
-	},
-	integrations: [
-		AstroPWA({
-			base: '/',
-			scope: '/',
-			includeAssets: ['favicon.svg'],
-			registerType: 'autoUpdate',
-			manifest: {
-				name: '가계부',
-				short_name: '가계부',
-				theme_color: '#ffffff',
-				icons: [{
-					src: 'icon-192x192.png',
-					sizes: '192x192',
-					type: 'image/png',
-				}, {
-					src: 'icon-512x512.png',
-					sizes: '512x512',
-					type: 'image/png',
-				}, {
-					src: 'icon-512x512.png',
-					sizes: '512x512',
-					type: 'image/png',
-					purpose: 'any maskable',
-				}],
-			},
-			workbox: {
-				navigateFallback: '/',
-				globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
-			},
-			experimental: {
-				directoryAndTrailingSlashHandler: true,
-			},
-		}),
-		auth(),
-	],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
+  vite: {
+    logLevel: 'info',
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`
+    },
+    server: {
+      fs: {
+        allow: ['../..']
+      }
+    }
+  },
+  integrations: [AstroPWA({
+    base: '/',
+    scope: '/',
+    includeAssets: ['favicon.svg'],
+    registerType: 'autoUpdate',
+    manifest: {
+      name: '가계부',
+      short_name: '가계부',
+      theme_color: '#ffffff',
+      icons: [{
+        src: 'icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      }, {
+        src: 'icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }, {
+        src: 'icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }]
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}']
+    },
+    experimental: {
+      directoryAndTrailingSlashHandler: true
+    }
+  }), 
+  auth(), 
+  react(), 
+  tailwind({
+    applyBaseStyles: false,
+  }),
+]
 });
